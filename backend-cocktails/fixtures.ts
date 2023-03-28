ed358ddaa5b30678faf6fc82050a32857463f302
omurbekov-azamat <avgusttin8@gmail.com>
2023-03-28 21:15:23 +0600

    created file multer.ts with settings and config.ts with index.ts with configuration inside;

---
+++ backend-cocktails/fixtures.ts
@@ -0,0 +1,34 @@
+import crypto from 'crypto';
+import mongoose from "mongoose";
+import config from "./config";
+import User from './modules/User';
+
+const run = async () => {
+    mongoose.set('strictQuery', false);
+    await mongoose.connect(config.db);
+    const db = mongoose.connection;
+
+    try {
+        await db.dropCollection('users');
+    } catch (e) {
+        console.log('Collections were not present');
+    }
+
+    await User.create({
+        username: 'user',
+        password: '123',
+        displayName: 'user',
+        avatar: null,
+        token: crypto.randomUUID(),
+    }, {
+        username: 'admin',
+        password: '123',
+        displayName: 'admin',
+        avatar: null,
+        token: crypto.randomUUID(),
+        role: 'admin'
+    });
+    await db.close();
+}
+
+void run();
--- backend-cocktails/index.ts
+++ backend-cocktails/index.ts
@@ -1,1 +1,26 @@
-console.log('hello world');
+import cors from 'cors';
+import express from "express";
+import mongoose from "mongoose";
+import config from "./config";
+
+const app = express();
+const port = 8000;
+
+app.use(cors());
+app.use(express.static('public'));
+app.use(express.json());
+
+const run = async () => {
+    mongoose.set('strictQuery', false);
+    await mongoose.connect(config.db);
+
+    app.listen(port, () => {
+        console.log('We are live on ' + port);
+    });
+
+    process.on('exit', () => {
+        mongoose.disconnect();
+    });
+};
+
+run().catch(console.error);
