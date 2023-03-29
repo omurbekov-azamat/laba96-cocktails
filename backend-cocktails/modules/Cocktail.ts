947d0ff48243178578718df1279b7afd02f8641c
omurbekov-azamat <avgusttin8@gmail.com>
2023-03-29 12:49:42 +0600

    created schema for cocktail;

---
+++ backend-cocktails/modules/Cocktail.ts
@@ -0,0 +1,40 @@
+import {model, Schema, Types} from 'mongoose';
+import User from './User';
+import {ICocktail} from '../types';
+
+const CocktailSchema = new Schema<ICocktail>({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+        validate: {
+            validator: async (value: Types.ObjectId) => User.findById(value),
+            message: 'User does not exist',
+        },
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    recipe: {
+        type: String,
+        required: true,
+    },
+    isPublished: {
+        type: Boolean,
+        required: true,
+        default: false,
+    },
+    ingredients: {
+        type: [String],
+        required: true,
+    },
+});
+
+const Cocktail = model<ICocktail>('Cocktail', CocktailSchema);
+
+export default Cocktail;
--- backend-cocktails/types.d.ts
+++ backend-cocktails/types.d.ts
@@ -1,3 +1,5 @@
+import {ObjectId} from 'mongoose';
+
 export interface IUser {
     email: string
     password: string;
@@ -6,4 +8,18 @@ export interface IUser {
     role: string;
     token: string;
     googleId?: string;
+}
+
+export interface Ingredient {
+    name: string;
+    amount: string;
+}
+
+export interface ICocktail {
+    user: ObjectId;
+    name: string;
+    image: string;
+    recipe: string;
+    isPublished: boolean;
+    ingredients: Ingredient[];
 }
